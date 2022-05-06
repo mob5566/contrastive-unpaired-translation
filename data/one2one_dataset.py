6ad0cb41ccda9628b5d5c3f6b634ccc005b06caa
code14 <mob5566@gmail.com>
2022-05-06 18:54:10 +0800

    models: add Self-inverse CycleGAN model and one-to-one dataset
    Signed-off-by: code14 <mob5566@gmail.com>

---
+++ data/one2one_dataset.py
@@ -0,0 +1,89 @@
+"""Dataset class template
+
+This module provides a template for users to implement custom datasets.
+You can specify '--dataset_mode template' to use this dataset.
+The class name should be consistent with both the filename and its dataset_mode option.
+The filename should be <dataset_mode>_dataset.py
+The class name should be <Dataset_mode>Dataset.py
+You need to implement the following functions:
+    -- <modify_commandline_options>:　Add dataset-specific options and rewrite default values for existing options.
+    -- <__init__>: Initialize this dataset class.
+    -- <__getitem__>: Return a data point and its metadata information.
+    -- <__len__>: Return the number of images.
+"""
+from data.base_dataset import BaseDataset, get_transform
+from data.image_folder import make_dataset
+from pathlib import Path
+from random import randint
+from PIL import Image
+
+
+class One2oneDataset(BaseDataset):
+    """A template dataset class for you to implement custom datasets."""
+    @staticmethod
+    def modify_commandline_options(parser, is_train):
+        """Add new dataset-specific options, and rewrite default values for existing options.
+
+        Parameters:
+            parser          -- original option parser
+            is_train (bool) -- whether training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+        """
+        return parser
+
+    def __init__(self, opt):
+        """Initialize this dataset class.
+
+        Parameters:
+            opt (Option class) -- stores all the experiment flags; needs to be a subclass of BaseOptions
+
+        A few things can be done here.
+        - save the options (have been done in BaseDataset)
+        - get image paths and meta information of the dataset.
+        - define the image transformation.
+        """
+        # save the option and dataset root
+        BaseDataset.__init__(self, opt)
+        # get the image paths of your dataset;
+        self.image_paths = [sorted(make_dataset(Path(self.root) / 'A',
+                                                opt.max_dataset_size)),
+                            sorted(make_dataset(Path(self.root) / 'B',
+                                                opt.max_dataset_size))]
+        self.image_idx = [(setid, idx) for setid in range(len(self.image_paths))
+                          for idx in range(len(self.image_paths[setid]))]
+        # define the default transform function. You can use <base_dataset.get_transform>; You can also define your custom transform function
+        self.transform = get_transform(opt)
+
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
+
+        Parameters:
+            index -- a random integer for data indexing
+
+        Returns:
+            a dictionary of data with their names. It usually contains the data itself and its metadata information.
+
+        Step 1: get a random image path: e.g., path = self.image_paths[index]
+        Step 2: load your data from the disk: e.g., image = Image.open(path).convert('RGB').
+        Step 3: convert your data to a PyTorch tensor. You can use helpder functions such as self.transform. e.g., data = self.transform(image)
+        Step 4: return a data point as a dictionary.
+        """
+        src_setid, src_idx = self.image_idx[index]
+        tgt_setid = src_setid ^ 1
+        tgt_idx = randint(0, len(self.image_paths[tgt_setid]) - 1)
+        path_A = self.image_paths[src_setid][src_idx]
+        path_B = self.image_paths[tgt_setid][tgt_idx]
+        img_A = Image.open(path_A).convert('RGB')
+        img_B = Image.open(path_B).convert('RGB')
+        data_A = self.transform(img_A)
+        data_B = self.transform(img_B)
+        dirt = src_setid
+        return {'A': data_A, 'B': data_B,
+                'A_paths': path_A, 'B_paths': path_B,
+                'direction': dirt}
+
+    def __len__(self):
+        """Return the total number of images."""
+        return len(self.image_idx)
---
+++ models/self_inverse_cycle_gan_model.py
@@ -0,0 +1,195 @@
+import torch
+import itertools
+from util.image_pool import ImagePool
+from .base_model import BaseModel
+from . import networks
+
+
+class SelfInverseCycleGANModel(BaseModel):
+    """
+    This class implements the self-inverse (or one-to-one) CycleGAN model, for learning image-to-image translation without paired data with one-to-one mapping constraint.
+
+    The model training requires '--dataset_mode one2one' dataset.
+    By default, the generator and discriminators are followed by CycleGAN.
+
+    Self-inverse CycleGAN paper: https://arxiv.org/pdf/1909.04110
+    """
+    @staticmethod
+    def modify_commandline_options(parser, is_train=True):
+        """Add new model-specific options and rewrite default values for existing options.
+
+        Parameters:
+            parser -- the option parser
+            is_train -- if it is training phase or test phase. You can use this flag to add training-specific or test-specific options.
+
+        Returns:
+            the modified parser.
+        """
+        parser.set_defaults(display_ncols=3)
+        parser.set_defaults(gan_mode='vanilla')
+        if is_train:
+            parser.add_argument('--lambda_A', type=float, default=10.0, help='weight for cycle loss (A -> B -> A)')
+            parser.add_argument('--lambda_B', type=float, default=10.0, help='weight for cycle loss (B -> A -> B)')
+
+        return parser
+
+    def __init__(self, opt):
+        """Initialize this model class.
+
+        Parameters:
+            opt -- training/test options
+
+        A few things can be done here.
+        - (required) call the initialization function of BaseModel
+        - define loss function, visualization images, model names, and optimizers
+        """
+        BaseModel.__init__(self, opt)  # call the initialization method of BaseModel
+        self.loss_names = ['D_A', 'G_A', 'cycle_A', 'D_B', 'G_B', 'cycle_B']
+        self.visual_names = ['real_A', 'fake_B', 'rec_A',
+                             'real_B', 'fake_A', 'rec_B']
+        self.model_names = ['G']
+        self.netG = networks.define_G(opt.input_nc, opt.output_nc, opt.ngf, opt.netG, opt.normG,
+                                      not opt.no_dropout, opt.init_type, opt.init_gain, opt.no_antialias, opt.no_antialias_up, self.gpu_ids, opt=opt)
+
+        if self.isTrain:  # define discriminators
+            self.netD_A = networks.define_D(opt.output_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.normD, opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt=opt)
+            self.netD_B = networks.define_D(opt.input_nc, opt.ndf, opt.netD,
+                                            opt.n_layers_D, opt.normD, opt.init_type, opt.init_gain, opt.no_antialias, self.gpu_ids, opt=opt)
+
+        if self.isTrain:
+            self.fake_A_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            self.fake_B_pool = ImagePool(opt.pool_size)  # create image buffer to store previously generated images
+            # define loss functions
+            self.criterionGAN = networks.GANLoss(opt.gan_mode).to(self.device)  # define GAN loss.
+            self.criterionCycle = torch.nn.L1Loss()
+            # initialize optimizers; schedulers will be automatically created by function <BaseModel.setup>.
+            self.optimizer_G = torch.optim.Adam(self.netG.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizer_D = torch.optim.Adam(itertools.chain(self.netD_A.parameters(), self.netD_B.parameters()), lr=opt.lr, betas=(opt.beta1, 0.999))
+            self.optimizers.append(self.optimizer_G)
+            self.optimizers.append(self.optimizer_D)
+
+        # Our program will automatically call <model.setup> to define schedulers, load networks, and print networks
+
+    def set_input(self, input):
+        """Unpack input data from the dataloader and perform necessary pre-processing steps.
+
+        Parameters:
+            input: a dictionary that contains the data itself and its metadata information.
+        """
+        self.real_A = input['A'].to(self.device)  # get image data A
+        self.real_B = input['B'].to(self.device)  # get image data B
+        self.direction = input['direction']
+        self.image_paths = input['A_paths']  # get image paths
+
+    def forward(self):
+        """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
+        self.fake_B = self.netG_A(self.real_A)  # G(A)
+        self.rec_A = self.netG_B(self.fake_B)   # G(G(A))
+        self.fake_A = self.netG_B(self.real_B)  # G(B)
+        self.rec_B = self.netG_A(self.fake_A)   # G(G(B))
+
+    def forward_A(self):
+        """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
+        self.fake_B = self.netG(self.real_A)  # G(A)
+        self.rec_A = self.netG(self.fake_B)   # G(G(A))
+
+    def forward_B(self):
+        """Run forward pass. This will be called by both functions <optimize_parameters> and <test>."""
+        self.fake_A = self.netG(self.real_B)  # G(B)
+        self.rec_B = self.netG(self.fake_A)   # G(G(B))
+
+    def backward_D_basic(self, netD, real, fake):
+        """Calculate GAN loss for the discriminator
+
+        Parameters:
+            netD (network)      -- the discriminator D
+            real (tensor array) -- real images
+            fake (tensor array) -- images generated by a generator
+
+        Return the discriminator loss.
+        We also call loss_D.backward() to calculate the gradients.
+        """
+        # Real
+        pred_real = netD(real)
+        loss_D_real = self.criterionGAN(pred_real, True)
+        # Fake
+        pred_fake = netD(fake.detach())
+        loss_D_fake = self.criterionGAN(pred_fake, False)
+        # Combined loss and calculate gradients
+        loss_D = (loss_D_real + loss_D_fake) * 0.5
+        loss_D.backward()
+        return loss_D
+
+    def backward_D_A(self):
+        """Calculate GAN loss for discriminator D_A"""
+        fake_B = self.fake_B_pool.query(self.fake_B)
+        self.loss_D_A = self.backward_D_basic(self.netD_A, self.real_B, fake_B)
+
+    def backward_D_B(self):
+        """Calculate GAN loss for discriminator D_B"""
+        fake_A = self.fake_A_pool.query(self.fake_A)
+        self.loss_D_B = self.backward_D_basic(self.netD_B, self.real_A, fake_A)
+
+    def backward_G_A(self):
+        """Calculate the loss for generators G_A and G_B"""
+        lambda_A = self.opt.lambda_A
+
+        # GAN loss D_A(G(A))
+        self.loss_G_A = self.criterionGAN(self.netD_A(self.fake_B), True)
+
+        # Forward cycle loss || G(G(A)) - A||
+        self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * lambda_A
+
+        # combined loss and calculate gradients
+        loss_G = self.loss_G_A + self.loss_cycle_A
+        loss_G.backward()
+
+    def backward_G_B(self):
+        """Calculate the loss for generators G_A and G_B"""
+        lambda_B = self.opt.lambda_B
+
+        # GAN loss D_A(G(B))
+        self.loss_G_B = self.criterionGAN(self.netD_B(self.fake_A), True)
+
+        # Forward cycle loss || G(G(B)) - B||
+        self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * lambda_B
+
+        # combined loss and calculate gradients
+        loss_G = self.loss_G_B + self.loss_cycle_B
+        loss_G.backward()
+
+    def data_dependent_initialize(self, data=None):
+        return
+
+    def generate_visuals_for_evaluation(self, data, mode):
+        with torch.no_grad():
+            visuals = {}
+            AtoB = self.opt.direction == "AtoB"
+            G = self.netG
+            source = data["A" if AtoB else "B"].to(self.device)
+            if mode == "forward":
+                visuals["fake_B"] = G(source)
+            else:
+                raise ValueError("mode %s is not recognized" % mode)
+            return visuals
+
+    def optimize_parameters(self):
+        """Calculate losses, gradients, and update network weights; called in every training iteration"""
+        self.set_requires_grad([self.netD_A, self.netD_B], False)  # Ds require no gradients when optimizing Gs
+        # G_A
+        self.forward_A()
+        self.optimizer_G.zero_grad()  # set G_A's gradients to zero
+        self.backward_G_A()             # calculate gradients for G_A and G_B
+        self.optimizer_G.step()       # update G_A's weights
+        # G_B
+        self.forward_B()
+        self.optimizer_G.zero_grad()  # set G_B's gradients to zero
+        self.backward_G_B()             # calculate gradients for G_A and G_B
+        self.optimizer_G.step()       # update G_B's weights
+        # D_A and D_B
+        self.set_requires_grad([self.netD_A, self.netD_B], True)
+        self.optimizer_D.zero_grad()   # set D_A and D_B's gradients to zero
+        self.backward_D_A()      # calculate gradients for D_A
+        self.backward_D_B()      # calculate graidents for D_B
+        self.optimizer_D.step()  # update D_A and D_B's weights
